f73c01eca88037921cc153b251255fc6a16ba0a4
Pierre <pierrefraisse90@gmail.com>
2023-05-25 10:15:45 +0200

    project 19

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,1 +1,29 @@
-console.log('dark mode');
+const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
+
+toggleBtn.addEventListener("click", () => {
+  console.log("hello");
+  document.documentElement.classList.toggle("dark-theme");
+});
+
+const articleData = articles
+  .map((article) => {
+    const { title, date, length, snippet } = article;
+    // format date
+    const formatDate = moment(date).format("MMMM Do, YYYY");
+    return `<article class="post">
+          <h2>${title}</h2>
+          <div class="post-info">
+            <span>${formatDate}</span>
+            <span>${length}</span>
+          </div>
+          <p>
+            ${snippet}
+          </p>
+        </article>`;
+  })
+  .join("");
+
+articlesContainer.innerHTML = articleData;
+
+// console.log(moment);
