3def9bb258c67d0d1f2927c6014cbeb04665721f
Pierre <pierrefraisse90@gmail.com>
2023-06-07 17:32:53 +0200

    complete : project 21

--- 21-dad-jokes/starter/app.js
+++ 21-dad-jokes/starter/app.js
@@ -1,1 +1,27 @@
-console.log('Dad Jokes Starter');
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+url = "https://icanhazdadjoke.com/";
+
+const fetchDadJoke = async () => {
+  result.textContent = "Loading...";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "learning app",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Whoops !");
+    }
+    const data = await response.json();
+    result.textContent = data.joke;
+  } catch (error) {
+    result.textContent = "Oopsy Daisy there were an error...";
+  }
+};
+
+window.addEventListener("load", fetchDadJoke);
+
+btn.addEventListener("click", fetchDadJoke);
